db4a1059b2852992aa6a11b29df707d78e8caf9f
Nikhil V <nikhilv@example.com>
2024-06-23 22:09:35 +0530

    WebConfig File

--- MyStock/src/main/java/com/example/MyStock/Config/WebConfig.java
+++ MyStock/src/main/java/com/example/MyStock/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","http://192.168.43.12:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                         .allowedHeaders("*")
                         .allowCredentials(true);
